8f424b0a55e3781abd80a932c0b0ca9498e3c807
Patrick <posten.coding@gmail.com>
2021-02-21 11:13:02 -0800

    alter the url gathering function to parse the url into multiple parts for future options and settings

--- js/background.js
+++ js/background.js
@@ -5,14 +5,30 @@ function Timer(){
   }, 1000)
 }
 
+// chrome.tabs.onActivated.addListener(function() {
+//   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+//     let url = tabs[0].url;
+//     if(url.includes("reddit")){
+//       Timer();
+//     }
+//   });
+// }); 
+
 chrome.tabs.onActivated.addListener(function() {
   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
     let url = tabs[0].url;
-    if(url.includes("reddit")){
+    console.log(url)
+    let urlArr = url.split('/');
+    console.log(urlArr)
+    let runTimerBoolArr = urlArr.map(e=> e.includes("reddit"));
+    console.log(runTimerBoolArr)
+    if(runTimerBoolArr.find(e=> e===true)){
       Timer();
     }
   });
 }); 
+
+
 //SAVE PREFERENCES BETWEEN CHROME RESTART
 // To read:
 
