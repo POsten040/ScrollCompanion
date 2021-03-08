f0bcf49d39ef4c9db79ea44545917da5415ad49d
Patrick <posten.coding@gmail.com>
2021-03-08 13:48:23 -0800

    add buttons for type of chrome.tabs method for url checking

--- background.js
+++ background.js
@@ -1,54 +1,98 @@
-
+const onOffState = {on: false} 
 let userInput = {
   minutes: null,
   customMin: null,
   domain: "",
   keywords: "",
-  topDomain: null
+  topDomain: null,
+  onNewTab: false,
+  onChangeTab: false
 }
 //Message comes from form button
 chrome.runtime.onMessage.addListener(onUserInput);
-function onUserInput(formMessage){
-  userInput = formMessage;
-  if(userInput.minutes == null && userInput.customMin != null){
-    userInput.minutes = userInput.customMin;
-    console.log(userInput)
-  } else if(userInput.customMin == null){
-    userInput.minutes = 1;
-    console.log(userInput);
+function onUserInput(message){
+  console.log(message)
+  if(message.minutes){
+    userInput = message;
+    if(userInput.minutes == null && userInput.customMin != null){
+      userInput.minutes = userInput.customMin;
+      console.log(userInput)
+    } else if(userInput.customMin == null){
+      userInput.minutes = 1;
+      console.log(userInput);
+    }
   }
+  // } else if (formMessage.on){
+  //   onOffState.on = formMessage.on;
+  // }
 }
 
 let tabId = null;
-
-//running in the background for matching urls
-chrome.tabs.onHighlighted.addListener(function() {
-  if(userInput.domain === null){
-    console.log("Scroll Alone")
-  } else {
-    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-      tabId = tabs[0].id;
-      let url = tabs[0].url;
-      let urlArr = url.split('/');
-      let domains = urlArr.splice(0, 3);
-      if(userInput.keywords != ""){
-        console.log(userInput.minutes);
-        if(domains.includes(userInput.domain) && urlArr.includes(userInput.keywords)){
-          chrome.alarms.create("userAlarm", {delayInMinutes: userInput.minutes});
-          console.log(chrome.alarms.get(userAlarm))
-        }
-      } else if (userInput.domain != ""){
-        if(domains.includes(userInput.domain)){
-          console.log(userInput.domains);
-          chrome.alarms.create("userAlarm", {delayInMinutes: userInput.minutes});
-          console.log(chrome.alarms.get(userAlarm))
-        }
+if(onOffState.on){
+  if(userInput.onNewTab){
+    chrome.tabs.onCreated.addListener(function() {
+      if(userInput.domain === null){
+        console.log("Scroll Alone")
       } else {
-        console.log("No Alarm Parameters Present")
+        chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+          tabId = tabs[0].id;
+          let url = tabs[0].url;
+          let urlArr = url.split('/');
+          let domains = urlArr.splice(0, 3);
+          if(userInput.keywords != ""){
+            console.log(userInput.minutes);
+            if(domains.includes(userInput.domain) && urlArr.includes(userInput.keywords)){
+              chrome.alarms.create("userAlarm", {delayInMinutes: userInput.minutes});
+              console.log(chrome.alarms.get(userAlarm))
+            }
+          } else if (userInput.domain != ""){
+            if(domains.includes(userInput.domain)){
+              console.log(userInput.domains);
+              chrome.alarms.create("userAlarm", {delayInMinutes: userInput.minutes});
+              console.log(chrome.alarms.get(userAlarm))
+            }
+          } else {
+            console.log("No Alarm Parameters Present")
+          }
+        });
       }
-    });
+    }); 
+  } else if(userInput.onChangeTab){
+    chrome.tabs.onHighlighted.addListener(function() {
+      if(userInput.domain === null){
+        console.log("Scroll Alone")
+      } else {
+        chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+          tabId = tabs[0].id;
+          let url = tabs[0].url;
+          let urlArr = url.split('/');
+          let domains = urlArr.splice(0, 3);
+          if(userInput.keywords != ""){
+            console.log(userInput.minutes);
+            if(domains.includes(userInput.domain) && urlArr.includes(userInput.keywords)){
+              chrome.alarms.create("userAlarm", {delayInMinutes: userInput.minutes});
+              console.log(chrome.alarms.get(userAlarm))
+            }
+          } else if (userInput.domain != ""){
+            if(domains.includes(userInput.domain)){
+              console.log(userInput.domains);
+              chrome.alarms.create("userAlarm", {delayInMinutes: userInput.minutes});
+              console.log(chrome.alarms.get(userAlarm))
+            }
+          } else {
+            console.log("No Alarm Parameters Present")
+          }
+        });
+      }
+    }); 
+  } else {
+    console.log("No tabs settings selected");
   }
-}); 
+} else {
+  console.log("it is off");
+}
+//running in the background for matching urls
+
 
 
 const options = {
--- js/popup.js
+++ js/popup.js
@@ -1,31 +1,61 @@
 
-
-let formSubmitButton = $("#formSubmit");
-formSubmitButton.click(function(){
+$("#formSubmit").click(function(){
   let formInput = {
     minutes: parseInt($('input:radio[name=minRadioButtons]:checked').val()),
     customMin: parseInt($("#customMin").val()),
     domain: $("#domain").val(),
     keywords: $("#keywords").val(),
-    topDomain: $("#topDomain").val()
+    topDomain: $("#topDomain").val(),
+    onNewTab: $("#onNewTabButton").val(),
+    onChangeTab: $("#onChangeTabButton").val()
   }
-  chrome.runtime.sendMessage(formInput)
+  chrome.runtime.sendMessage(formInput);
+  $("#displayDomains").append("<li>" + formInput.domain + "</li>");
+  $("#domain").text("");
 })
+
+$("#clearSettings").click(function(){
+  $("#domain").text("");
+  $("#displayDomains").text("");
+  $("#keywords").text("");
+  $("#topDomain").text("");
+  const resetSettings = {
+    minutes: null,
+    customMin: null,
+    domain: "",
+    keywords: "",
+    topDomain: null,
+    onNewTab: false,
+    onChangeTab: false
+  }
+  chrome.runtime.sendMessage(resetSettings);
+})
+
 
 $("#darkModeButton").click(function(){
   $("#holder").toggleClass("darkMode");
   $("#holder").toggleClass("lightMode");
+  if($("#darkModeButton").hasClass("off")){
+    $("#darkModeButton").toggleClass("off").text("Light Mode");
+    $("#darkModeButton").toggleClass("on");
+  } else {
+    $("#darkModeButton").toggleClass("on").text("Dark Mode");
+    $("#darkModeButton").toggleClass("off");
+  }
 })
 
 $("#onButton").click(function(){
   if($("#onButton").hasClass("off")){
     $("#onButton").toggleClass("off").text("On");
     $("#onButton").toggleClass("on");
+    chrome.runtime.sendMessage({on:true});
   } else {
     $("#onButton").toggleClass("on").text("Off");
     $("#onButton").toggleClass("off");
+    chrome.runtime.sendMessage({on:false})
   }
 })
+
 // document.getElementById("modal").addEventListener("click", function() {
 //   console.log("submitted!")
 // }); 
